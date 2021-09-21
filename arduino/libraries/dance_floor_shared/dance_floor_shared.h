9c3725717104f473672c894c47ad16fda835b9d8
Michelle Sharer <michellesharer@gmail.com>
2021-09-21 13:41:27 -0600

    The actual corrected order of strand lengths after repairing broken LEDs

--- arduino/libraries/dance_floor_shared/dance_floor_shared.h
+++ arduino/libraries/dance_floor_shared/dance_floor_shared.h
@@ -24,40 +24,40 @@
 #define VIZ_JUGGLE     6
 
 // Blue
-#define LEDS_6   180  // 30
-#define LEDS_5   140  // -10
-#define LEDS_4   124  // -26
+#define LEDS_0   180  // 30
+#define LEDS_1   140  // -10
+#define LEDS_2   124  // -26
 #define LEDS_3   122  // -28
-#define LEDS_2   122  // -28
-#define LEDS_1   127  // -23
-#define LEDS_0   134  // -16
+#define LEDS_4   122  // -28
+#define LEDS_5   127  // -23
+#define LEDS_6   134  // -16
 
 // Green
-#define LEDS_13  207  // 57
-#define LEDS_12  181  // 31
-#define LEDS_11  170  // 20
+#define LEDS_7   207  // 57
+#define LEDS_8   181  // 31
+#define LEDS_9   170  // 20
 #define LEDS_10  166  // 16
-#define LEDS_9   166  // 16
-#define LEDS_8   177  // 27
-#define LEDS_7   196  // 46
+#define LEDS_11  166  // 16
+#define LEDS_12  177  // 27
+#define LEDS_13  196  // 46
 
 // Yellow
-#define LEDS_20  195  // 45
-#define LEDS_19  175  // 25
-#define LEDS_18  170  // 20
+#define LEDS_14  195  // 45
+#define LEDS_15  175  // 25
+#define LEDS_16  170  // 20
 #define LEDS_17  168  // 18
-#define LEDS_16  173  // 23
-#define LEDS_15  178  // 28
-#define LEDS_14  205  // 55
+#define LEDS_18  173  // 23
+#define LEDS_19  178  // 28
+#define LEDS_20  205  // 55
 
 // Red
-#define LEDS_27  144  // -6
-#define LEDS_26  131  // -19
-#define LEDS_25  128  // -22
+#define LEDS_21  144  // -6
+#define LEDS_22  131  // -19
+#define LEDS_23  128  // -22
 #define LEDS_24  128  // -22
-#define LEDS_23  134  // -16
-#define LEDS_22  144  // -6
-#define LEDS_21  190  // 40
+#define LEDS_25  134  // -16
+#define LEDS_26  144  // -6
+#define LEDS_27  190  // 40
 
 int NUM_LEDS[] = {
   LEDS_0, LEDS_1, LEDS_2, LEDS_3, LEDS_4, LEDS_5, LEDS_6,
